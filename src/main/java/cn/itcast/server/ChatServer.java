718c84990304aa125c4de2a171ca17cf7cfa3666
王玉东 <1174872613@qq.com>
2023-01-28 17:38:12 +0800

    聊天室心跳fix

--- src/main/java/cn/itcast/server/ChatServer.java
+++ src/main/java/cn/itcast/server/ChatServer.java
@@ -49,7 +49,7 @@ public class ChatServer {
                     ch.pipeline().addLast(MESSAGE_CODEC);
                     // 用来判断是不是 读空闲时间过长，或 写空闲时间过长
                     // 5s 内如果没有收到 channel 的数据，会触发一个 IdleState#READER_IDLE 事件
-                    ch.pipeline().addLast(new IdleStateHandler(7, 0, 0));
+                    ch.pipeline().addLast(new IdleStateHandler(5, 0, 0));
                     // ChannelDuplexHandler 可以同时作为入站和出站处理器
                     ch.pipeline().addLast(new ChannelDuplexHandler() {
                         // 用来触发特殊事件
